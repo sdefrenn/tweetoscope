f5a094e6177879e02ac8f99de465c245e647d501
Wakaran <zenwakan@gmail.com>
2022-03-18 15:46:25 +0100

    all scopes in

--- server/src/twitter.strategy.ts
+++ server/src/twitter.strategy.ts
@@ -10,13 +10,15 @@ export class TwitterStrategy extends PassportStrategy(Strategy, 'twitter') {
       clientID: 'T05Jb3hlTWhYSy1DWlJycGsyM286MTpjaQ',
       clientSecret: 'ZMOK6KOzGq1-YRTe3Jr_4b-rdS2AuKt1zhq0d4fvgRSBtEbPz9',
       callbackURL: 'http://127.0.0.1:3000/twitter/redirect',
-      scope: ['tweet.read'],
+      scope: ["tweet.read","tweet.write","tweet.moderate.write","users.read","follows.read","follows.write","offline.access","space.read","mute.read","mute.write","like.read","like.write","list.read","list.write","block.read","block.write"],
     });
   }
+
 
   async validate(
     accessToken: string,
     refreshToken: string,
+    profile: any,
     done: VerifyCallback,
   ): Promise<any> {
     console.log(accessToken);
