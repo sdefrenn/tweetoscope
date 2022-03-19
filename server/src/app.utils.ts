a273864b1681d6179c8a0012c9e3529869d282c4
Sifortin <33554200+Sifortin@users.noreply.github.com>
2022-03-19 16:49:29 +0100

    modfied AES256 ctr to AES256 cbc

--- server/src/app.utils.ts
+++ server/src/app.utils.ts
@@ -1,26 +1,23 @@
-import { createCipheriv, randomBytes, scrypt } from 'crypto';
-import { promisify } from 'util';
+import { createCipheriv, randomBytes } from 'crypto';
 import { createDecipheriv } from 'crypto';
 
-export async function AESCipher(mes, pwd) {
+export async function AESCipher(mes) {
   // random bytes added
+
+  //don't know what to do with the value, should it be constant ?
   const iv = randomBytes(16);
-  const password = pwd;
-  const salt = randomBytes(128);
+  const key = randomBytes(16).toString('hex');
   // The key length is dependent on the algorithm.
   // In this case for aes256, it is 32 bytes.
-  const key = (await promisify(scrypt)(password, salt, 32)) as Buffer;
-  const cipher = createCipheriv('aes-256-ctr', key, iv);
-  const textToEncrypt = mes;
-  const encryptedText = Buffer.concat([
-    cipher.update(textToEncrypt),
-    cipher.final(),
-  ]);
-  return [encryptedText, key, iv];
+
+  const cipher = createCipheriv('aes-256-cbc', key, iv);
+  let encryptedText = cipher.update(mes, 'utf8', 'base64');
+  encryptedText += cipher.final('base64');
+  return { encryptedText, iv, key };
 }
 
 export function AESDecipher(mes, key, iv) {
-  const decipher = createDecipheriv('aes-256-ctr', key, iv);
-  const decryptedText = Buffer.concat([decipher.update(mes), decipher.final()]);
-  return decryptedText;
+  const decipher = createDecipheriv('aes-256-cbc', key, iv);
+  const decryptedText = decipher.update(mes, 'base64', 'utf8');
+  return decryptedText + decipher.final('utf8');
 }
--- server/src/twitter.strategy.ts
+++ server/src/twitter.strategy.ts
@@ -2,6 +2,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from '@superfaceai/passport-twitter-oauth2';
 
 import { Injectable } from '@nestjs/common';
+import { AESCipher } from './app.utils';
 
 @Injectable()
 export class TwitterStrategy extends PassportStrategy(Strategy, 'twitter') {
@@ -10,18 +11,37 @@ export class TwitterStrategy extends PassportStrategy(Strategy, 'twitter') {
       clientID: 'T05Jb3hlTWhYSy1DWlJycGsyM286MTpjaQ',
       clientSecret: 'ZMOK6KOzGq1-YRTe3Jr_4b-rdS2AuKt1zhq0d4fvgRSBtEbPz9',
       callbackURL: 'http://127.0.0.1:3000/twitter/redirect',
-      scope: ["tweet.read","tweet.write","tweet.moderate.write","users.read","follows.read","follows.write","offline.access","space.read","mute.read","mute.write","like.read","like.write","list.read","list.write","block.read","block.write"],
+      scope: [
+        'tweet.read',
+        'tweet.write',
+        'tweet.moderate.write',
+        'users.read',
+        'follows.read',
+        'follows.write',
+        'offline.access',
+        'space.read',
+        'mute.read',
+        'mute.write',
+        'like.read',
+        'like.write',
+        'list.read',
+        'list.write',
+        'block.read',
+        'block.write',
+      ],
     });
   }
-
 
   async validate(
     accessToken: string,
     refreshToken: string,
     profile: any,
     done: VerifyCallback,
-  ): Promise<{token: string, refresh: string}> {
+  ): Promise<{ token: any; refresh: any }> {
     //maybe encrypt from here instead?
-   return {token: accessToken, refresh: refreshToken}; 
+
+    let tokenAES = AESCipher(accessToken);
+    let refreshAES = AESCipher(refreshToken);
+    return { token: tokenAES, refresh: refreshAES };
   }
 }
