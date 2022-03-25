9b0129dbe2138b588c8fdf5be49e294b95833a13
Wakaran <zenwakan@gmail.com>
2022-03-25 13:18:11 +0100

    missing style arguments

--- client/src/containers/tweetNode/tweetNode.tsx
+++ client/src/containers/tweetNode/tweetNode.tsx
@@ -1,11 +1,11 @@
 import DisplayTweet from "../../commons/models/diplayTweet";
 import { TweetDiv } from "./styles";
 
-function TweetNode({ data, nameColor, textColor, backgroundColor, borderColor }:
+function TweetNode({ data, nameColor, textColor, backgroundColor, borderColor}:
     {data: DisplayTweet, color?: string, nameColor?: string, textColor?: string, backgroundColor?: string, borderColor?: string}) {
 
   return(
-    <TweetDiv backgroundColor={backgroundColor!} borderColor={borderColor!} pos={data.position} dimension={data.dimension}>
+    <TweetDiv backgroundColor={backgroundColor!} borderColor={borderColor!} pos={data.position} dimensions={data.dimension}>
       <p style={{ color: nameColor }}>{data.name} @{data.username} - {data.stringDate}</p>
       <br/>
       <p style={{ color: textColor }}>{data.text}</p>
