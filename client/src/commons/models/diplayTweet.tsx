b948158c9b0e5bd621e65fe2fa1a449fbe9d28d1
Wakaran <zenwakan@gmail.com>
2022-03-29 01:26:25 +0200

    remember span, ref instead of subclass

--- client/src/commons/models/diplayTweet.tsx
+++ client/src/commons/models/diplayTweet.tsx
@@ -1,21 +1,31 @@
 import Tweet from "./tweet";
 
-class DisplayTweet extends Tweet{
+class DisplayTweet{
 
     position: {x: number, y: number};
     dimension: {width: number, height: number};
 
+    subtreeSpan: {startX: number, endX: number};
+
     private _displayChildren: DisplayTweet[];
     private _displayParent: DisplayTweet|null;
+    private _hidden: boolean;
+
+    private _tweet: Tweet;
 
     constructor(tweet: Tweet, position: {x: number, y: number}, displayParent?: DisplayTweet, displayChildren?: DisplayTweet[]){
-        super(tweet.id, tweet.name, tweet.username, tweet.date, tweet.text, tweet.parent, tweet.replies);
+
+        this._tweet = tweet;
 
         this.position = position;
         this.dimension = {width: 400, height: 100};
+        this.subtreeSpan = {startX: this.position.x, endX: this.position.x+this.dimension.width};
 
         this._displayParent = displayParent?displayParent:null;
         this._displayChildren = displayChildren?displayChildren:[];
+
+        this._hidden = false;
+
     }
 
     addDisplayChild(child: DisplayTweet){
@@ -31,6 +41,33 @@ class DisplayTweet extends Tweet{
 
     get displayParent(){
         return this._displayParent;
+    }
+
+    setHidden(state: boolean){
+        this._hidden = state;
+    }
+
+    get isHidden(){
+        return this._hidden;
+    }
+
+
+    //get data from tweet (not a subclass to avoid redundant data)
+    //modifying the tweet would update the display as well
+    get id(){
+        return this._tweet.id;
+    }
+    get text(){
+        return this._tweet.text;
+    }
+    get stringDate(){
+        return this._tweet.stringDate;
+    }
+    get username(){
+        return this._tweet.username;
+    }
+    get name(){
+        return this._tweet.name;
     }
 
 
