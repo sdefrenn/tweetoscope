afd656bdabe7580cbf042fdaf830da9b18fd3eb3
Wakaran <zenwakan@gmail.com>
2022-03-29 01:56:36 +0200

    bigger test

--- client/src/commons/services/twitterService.tsx
+++ client/src/commons/services/twitterService.tsx
@@ -28,8 +28,15 @@ class TwitterService{
             new Tweet(nextId.next().value!.toString(), "Justine Dupont", "dptju2000", new Date(2022, 3, 4, 10, 15), "Omg Mary hi!!! Great new member for Twitter!", temp_tweet),
             new Tweet(nextId.next().value!.toString(),"Robotott", "robyy", new Date(2022, 3, 3, 15, 9), "Hello Mary Sue!", temp_tweet)
         ];
-    
-        temp_res[0].addReply(new Tweet(nextId.next().value!.toString(),"Hamidoty", "hamidd57", new Date(2022, 3, 3, 15, 9), "I agree, Bob. Welcome to Twitter Mary Sue!", temp_res[0]));
+        let t = new Tweet(nextId.next().value!.toString(),"Hamidoty", "hamidd57", new Date(2022, 3, 3, 15, 9), "I agree, Bob. Welcome to Twitter Mary Sue!", temp_res[0])
+        t.addReply(new Tweet(nextId.next().value!.toString(),"Hamidoty", "hamidd57", new Date(2022, 3, 3, 15, 9), "I agree, Bob. Welcome to Twitter Mary Sue!", temp_res[0]));
+        t.addReply(new Tweet(nextId.next().value!.toString(),"Hamidoty", "hamidd57", new Date(2022, 3, 3, 15, 9), "I agree, Bob. Welcome to Twitter Mary Sue!", temp_res[0]));
+        let t2 = new Tweet(nextId.next().value!.toString(),"Hamidoty", "hamidd57", new Date(2022, 3, 3, 15, 9), "I agree, Bob. Welcome to Twitter Mary Sue!", temp_res[0])
+        t.addReply(new Tweet(nextId.next().value!.toString(),"Hamidoty", "hamidd57", new Date(2022, 3, 3, 15, 9), "I agree, Bob. Welcome to Twitter Mary Sue!", temp_res[0]));
+        t.addReply(new Tweet(nextId.next().value!.toString(),"Hamidoty", "hamidd57", new Date(2022, 3, 3, 15, 9), "I agree, Bob. Welcome to Twitter Mary Sue!", temp_res[0]));
+        temp_res[0].addReply(t);
+        temp_res[0].addReply(t2);
+        
         temp_res.forEach(tweet => temp_tweet!.addReply(tweet));
     
     
